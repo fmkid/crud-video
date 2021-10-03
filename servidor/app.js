ace286bd85d113899627260d7e6afc8976e686d6
Ffmkid <fabian.morecam@gmail.com>
2021-10-03 18:50:23 -0500

    Prueba CRUD MEVN siguiendo el video de youtube de Creación de un CRUD desde cero

---
+++ servidor/app.js
@@ -0,0 +1,44 @@
+// const express = require('express');
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const path = require('path');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+// Conexión base de datos
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost:27017/myapp';
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+// Or using promises
+mongoose.connect(uri, options).then(
+    /** ready to use. The `mongoose.connect()` promise resolves to 
+   mongoose instance. */
+    () => { console.log('Conectado a DB') },
+    /** handle initial connection error */
+    err => { console.log(err) }
+);
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rutas
+app.use('/api', require('./routes/nota'));
+
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Puerto
+app.set('puerto', process.env.PORT || 3000);
+
+app.listen(app.get('puerto'), function () {
+    console.log('Servidor escuchando por el puerto ' + app.get('puerto'));
+})
